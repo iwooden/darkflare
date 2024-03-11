20cf17c80698816b40affbcb440bdb0ae65e83c3
Isaac Wooden <iwooden@Isaacs-MBP.home>
2024-03-11 11:54:38 -0700

    small cleanup

--- backend/src/controller/RangeController.ts
+++ backend/src/controller/RangeController.ts
@@ -1,8 +1,6 @@
 import { AppDataSource } from "../data-source"
-import { PartyCreate, PartyDelete, PartyQuery } from "../validator/PartyValidators"
 import { ReqBody, ReqQuery } from "../util/reqTypes"
 import { Range } from "../entity/Range"
-import { Character } from "../entity/Character"
 import { RangeQuery } from "../validator/RangeValidators"
 
 export class RangeController {
